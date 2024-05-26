dacc2811698a1184e01ee598bd9c87208d6fabae
elen-oz <ozerova.lenka@gmail.com>
2024-05-26 18:48:28 +0200

    feat: change view depending on user

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -12,26 +12,36 @@ const ChatMessage = ({ message, userName }) => {
     }
   }, [createdAt]);
 
-  const messageClass =
+  const messageColorClass =
     uid === auth.currentUser.uid
       ? 'bg-green-200'
-      : 'bg-violet-200 justify-self-end';
+      : 'bg-violet-200';
+  const messagePlaceClass =
+  uid === auth.currentUser.uid
+    ? 'flex-row-reverse'
+    : 'flex-row';
+  const messageNamePlaceClass =
+  uid === auth.currentUser.uid
+    ? 'text-end'
+    : 'text-start';
 
   return (
-    <div className='mb-2 flex items-center justify-start gap-x-2 px-2'>
-      <div className='flex w-[40px] flex-col items-center justify-center'>
+    <div className={`mb-2 flex ${messagePlaceClass} items-end justify-start gap-x-2 px-2`}>
+      <div className='flex w-[60px] flex-col items-center justify-center'>
         <img src={photoURL || '/user.webp'} className='w-full rounded-full' />
-        {/* <p>{userName}</p> */}
       </div>
 
-      <div className='flex w-full flex-row items-end justify-between gap-x-4'>
-        <div
-          className={`${messageClass} w-fit rounded-3xl px-4 py-2 shadow-lg`}
-        >
-          {text}
-        </div>
-        <div className='whitespace-nowrap py-2 text-sm italic text-stone-400'>
-          {formattedTime}
+      <div className='w-full'>
+      <p className={`text-sm text-stone-500 ${messageNamePlaceClass}`}>{userName}</p>
+        <div className={`flex ${messagePlaceClass} items-end justify-between gap-x-4`}>
+          <div
+            className={`${messageColorClass} w-fit rounded-3xl px-4 py-2 shadow-lg`}
+          >
+            {text}
+          </div>
+          <div className='whitespace-nowrap py-2 text-sm italic text-stone-400'>
+            {formattedTime}
+          </div>
         </div>
       </div>
     </div>
